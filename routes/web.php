0388976a1ce73785484d29f0c2b09e9cbcf66665
vertexos1 <allensanchez1628@gmail.com>
2024-12-11 21:08:37 +0800

    1h23m2s designing products detail

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Livewire\ProductDetails;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product/details',ProductDetails::class);
